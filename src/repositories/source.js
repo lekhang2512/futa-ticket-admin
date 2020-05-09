e6959f0adab43804c663be52902cc9539ac72e9a
AnhPta <tuananhsc96@gmail.com>
2020-05-09 10:36:43 +0700

    delete repo

--- src/repositories/source.js
+++ src/repositories/source.js
@@ -4,15 +4,6 @@ class TicketRepository extends BaseRepository {
   url () {
     return this.apiVersion() + '/sources'
   }
-
-  async update (id, data, query = {}, headers = {}) {
-    try {
-      let response = await this.httpClient.put(this.url() + '/' + id, data, { params: query, headers: headers })
-      return this.success(response.data)
-    } catch (e) {
-      return this.handlerHttpError(e)
-    }
-  }
 }
 
 export default TicketRepository
